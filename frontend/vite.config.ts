a72b1ba58eb7d3f574f0ee822493beeb1ee5af08
Jesús Aguirre <jaguirrepeman@gmail.com>
2026-06-07 13:48:07 +0200

    fix: api dir

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -63,6 +63,10 @@ export default defineConfig({
         target: "http://localhost:8000",
         changeOrigin: true,
       },
+      "/finance/api": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+      },
     },
   },
   build: {
